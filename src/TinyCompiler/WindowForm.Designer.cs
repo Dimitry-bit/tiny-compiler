c366d5614427766d0320c0dfa23d0458c94fa0ef
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-12-18 22:13:29 +0200

    feat(form): hookup parser to WindowForm and line number gutter

--- src/TinyCompiler/WindowForm.Designer.cs
+++ src/TinyCompiler/WindowForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tfSourceCode = new System.Windows.Forms.TextBox();
             this.btnCompile = new System.Windows.Forms.Button();
             this.tblTokens = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -37,26 +36,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.tfErrors = new System.Windows.Forms.TextBox();
+            this.treePrase = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tfSourceCode = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.tblTokens)).BeginInit();
             this.SuspendLayout();
             // 
-            // tfSourceCode
-            // 
-            this.tfSourceCode.Location = new System.Drawing.Point(26, 44);
-            this.tfSourceCode.Margin = new System.Windows.Forms.Padding(2);
-            this.tfSourceCode.Multiline = true;
-            this.tfSourceCode.Name = "tfSourceCode";
-            this.tfSourceCode.Size = new System.Drawing.Size(278, 452);
-            this.tfSourceCode.TabIndex = 0;
-            this.tfSourceCode.WordWrap = false;
-            // 
             // btnCompile
             // 
-            this.btnCompile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnCompile.Location = new System.Drawing.Point(26, 504);
+            this.btnCompile.Location = new System.Drawing.Point(47, 615);
             this.btnCompile.Margin = new System.Windows.Forms.Padding(2);
             this.btnCompile.Name = "btnCompile";
-            this.btnCompile.Size = new System.Drawing.Size(278, 31);
+            this.btnCompile.Size = new System.Drawing.Size(263, 31);
             this.btnCompile.TabIndex = 3;
             this.btnCompile.Text = "Compile";
             this.btnCompile.UseVisualStyleBackColor = true;
@@ -67,20 +59,22 @@
             this.tblTokens.AllowUserToAddRows = false;
             this.tblTokens.AllowUserToDeleteRows = false;
             this.tblTokens.AllowUserToResizeRows = false;
-            this.tblTokens.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblTokens.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tblTokens.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.tblTokens.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.tblTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tblTokens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.tblTokens.Location = new System.Drawing.Point(329, 44);
-            this.tblTokens.Margin = new System.Windows.Forms.Padding(2);
+            this.tblTokens.Location = new System.Drawing.Point(432, 32);
+            this.tblTokens.Margin = new System.Windows.Forms.Padding(8);
             this.tblTokens.Name = "tblTokens";
             this.tblTokens.ReadOnly = true;
             this.tblTokens.RowHeadersWidth = 50;
             this.tblTokens.RowTemplate.Height = 24;
-            this.tblTokens.Size = new System.Drawing.Size(429, 270);
+            this.tblTokens.Size = new System.Drawing.Size(400, 282);
             this.tblTokens.TabIndex = 4;
             // 
             // Column1
@@ -99,7 +93,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(26, 24);
+            this.label1.Location = new System.Drawing.Point(44, 12);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 18);
@@ -108,10 +102,12 @@
             // 
             // label2
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(326, 24);
+            this.label2.Location = new System.Drawing.Point(429, 12);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 18);
@@ -120,9 +116,12 @@
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(326, 342);
+            this.label4.Location = new System.Drawing.Point(429, 322);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 18);
@@ -131,29 +130,79 @@
             // 
             // tfErrors
             // 
+            this.tfErrors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tfErrors.BackColor = System.Drawing.SystemColors.Control;
             this.tfErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tfErrors.ForeColor = System.Drawing.Color.Red;
-            this.tfErrors.Location = new System.Drawing.Point(329, 363);
+            this.tfErrors.Location = new System.Drawing.Point(432, 348);
+            this.tfErrors.Margin = new System.Windows.Forms.Padding(8);
             this.tfErrors.Multiline = true;
             this.tfErrors.Name = "tfErrors";
             this.tfErrors.ReadOnly = true;
             this.tfErrors.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tfErrors.Size = new System.Drawing.Size(429, 133);
+            this.tfErrors.Size = new System.Drawing.Size(400, 257);
             this.tfErrors.TabIndex = 10;
+            // 
+            // treePrase
+            // 
+            this.treePrase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.treePrase.Location = new System.Drawing.Point(848, 32);
+            this.treePrase.Margin = new System.Windows.Forms.Padding(8);
+            this.treePrase.Name = "treePrase";
+            this.treePrase.Size = new System.Drawing.Size(400, 573);
+            this.treePrase.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(845, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Parse Tree";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(315, 615);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 31);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tfSourceCode
+            // 
+            this.tfSourceCode.BackColor = System.Drawing.SystemColors.Window;
+            this.tfSourceCode.DetectUrls = false;
+            this.tfSourceCode.Location = new System.Drawing.Point(47, 32);
+            this.tfSourceCode.Margin = new System.Windows.Forms.Padding(8);
+            this.tfSourceCode.Name = "tfSourceCode";
+            this.tfSourceCode.Size = new System.Drawing.Size(369, 573);
+            this.tfSourceCode.TabIndex = 15;
+            this.tfSourceCode.Text = "";
+            this.tfSourceCode.WordWrap = false;
             // 
             // WindowForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.tfSourceCode);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.treePrase);
             this.Controls.Add(this.tfErrors);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tblTokens);
             this.Controls.Add(this.btnCompile);
-            this.Controls.Add(this.tfSourceCode);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
@@ -169,8 +218,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tfSourceCode;
         private System.Windows.Forms.Button btnCompile;
         private System.Windows.Forms.DataGridView tblTokens;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
@@ -179,5 +226,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox tfErrors;
+        private System.Windows.Forms.TreeView treePrase;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox tfSourceCode;
     }
 }
--- src/TinyCompiler/WindowForm.cs
+++ src/TinyCompiler/WindowForm.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System;
+using System.Windows.Forms;
 
 namespace TinyCompiler
 {
@@ -7,6 +9,31 @@ namespace TinyCompiler
         public WindowForm()
         {
             InitializeComponent();
+
+            // Subscribe to the necessary events
+            tfSourceCode.TextChanged += (object sender, EventArgs e) => Invalidate();
+            tfSourceCode.VScroll += (object sender, EventArgs e) => Invalidate();
+            tfSourceCode.KeyDown += RichTextBox1_KeyDown;
+        }
+
+        private void RichTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Check if Ctrl+V is pressed (paste command)
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                // Check if clipboard contains plain text
+                if (Clipboard.ContainsText())
+                {
+                    // Get plain text from clipboard
+                    string plainText = Clipboard.GetText(TextDataFormat.Text);
+
+                    // Insert the plain text into the RichTextBox (at current cursor position)
+                    tfSourceCode.SelectedText = plainText;
+                }
+
+                // Prevent default paste behavior (i.e., disabling rich text paste)
+                e.SuppressKeyPress = true;
+            }
         }
 
         private void btnCompile_Click(object sender, System.EventArgs e)
@@ -15,6 +42,7 @@ namespace TinyCompiler
             Compiler.Compile(sourceCode);
 
             PopulateTokensTable();
+            PopulateParseTree();
             PrintErrors();
         }
 
@@ -33,6 +61,98 @@ namespace TinyCompiler
             foreach (var error in Errors.Error_List)
             {
                 tfErrors.Text += error + "\r\n";
+            }
+        }
+
+        private void PopulateParseTree()
+        {
+            treePrase.Nodes.Clear();
+            treePrase.Nodes.Add(PrintParaseTree(Compiler.treeRoot));
+        }
+
+        private void btnClear_Click(object sender, System.EventArgs e)
+        {
+            // Resest UI Elements
+            tfSourceCode.Clear();
+            tfErrors.Clear();
+            tblTokens.Rows.Clear();
+            treePrase.Nodes.Clear();
+
+            // Reset Global Data
+            Compiler.TokenStream.Clear();
+            Errors.Error_List.Clear();
+        }
+
+        private static TreeNode PrintParaseTree(Node root)
+        {
+            TreeNode tree = new TreeNode("Parse Tree");
+            TreeNode treeRoot = PrintTree(root);
+
+            if (treeRoot != null)
+            {
+                tree.Nodes.Add(treeRoot);
+            }
+
+            return tree;
+        }
+
+        private static TreeNode PrintTree(Node root)
+        {
+            if ((root == null) || (root.Name == null))
+            {
+                return null;
+            }
+
+            TreeNode tree = new TreeNode(root.Name);
+            foreach (Node child in root.Children)
+            {
+                if (child == null)
+                {
+                    continue;
+                }
+
+                tree.Nodes.Add(PrintTree(child));
+            }
+
+            return tree;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawLineNumbers(e.Graphics);
+        }
+
+        private void DrawLineNumbers(Graphics graphics)
+        {
+            // Set the font and brush for the line numbers
+            Font lineNumberFont = new Font("Consolas", 10);
+            Brush lineNumberBrush = Brushes.Gray;
+
+            // Calculate the height of a single line based on the font
+            // Use GetLineFromCharIndex and GetPositionFromCharIndex to get the correct line height
+            int lineHeight =  tfSourceCode.GetPositionFromCharIndex(tfSourceCode.GetFirstCharIndexFromLine(1)).Y
+                          - tfSourceCode.GetPositionFromCharIndex(tfSourceCode.GetFirstCharIndexFromLine(0)).Y;
+
+            // Get the first and last visible lines
+            int firstVisibleLine = tfSourceCode.GetLineFromCharIndex(tfSourceCode.GetCharIndexFromPosition(new Point(0, 4)));
+            int lastVisibleLine = tfSourceCode.GetLineFromCharIndex(tfSourceCode.GetCharIndexFromPosition(new Point(0, tfSourceCode.ClientRectangle.Bottom)));
+
+            // Get the position of the RichTextBox control (Top is the distance from the top of the form)
+            int richTextBoxTop = tfSourceCode.Top;
+            int richTextBoxLeft = tfSourceCode.Left;
+
+            // Set the initial position for drawing the line numbers
+            // Align with the left of the RichTextBox
+            int xPosition = richTextBoxLeft;
+            int yPosition = richTextBoxTop + tfSourceCode.GetPositionFromCharIndex(tfSourceCode.GetFirstCharIndexFromLine(firstVisibleLine)).Y;
+
+            // Draw the line numbers for all visible lines
+            for (int i = firstVisibleLine; i <= lastVisibleLine; i++)
+            {
+                SizeF size = graphics.MeasureString((i + 1).ToString(), lineNumberFont);
+                graphics.DrawString((i + 1).ToString(), lineNumberFont, lineNumberBrush, xPosition - size.Width, yPosition);
+                yPosition += lineHeight;  // Move to the next line position
             }
         }
     }
